3cbd384c6e2037a460afdb10ca4df4e5b1ff88f9
akausinis <andrius.kausinis@singleton-labs.lt>
2017-05-23 18:01:21 +0300

    Adding IBM B2Bi streaming plugin prototype implementation.

---
+++ bin/tnt4j-streams.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+if command -v realpath >/dev/null 2>&1; then
+    SCRIPTPATH=`dirname $(realpath $0)`
+else
+    SCRIPTPATH=$( cd "$(dirname "$0")" ; pwd -P )
+fi
+
+RUNDIR=`pwd`
+LIBPATH="$SCRIPTPATH/../*:$SCRIPTPATH/../lib/*"
+TNT4JOPTS="-Dtnt4j.config=$SCRIPTPATH/../config/tnt4j.properties"
+LOG4JOPTS="-Dlog4j.configuration=file:$SCRIPTPATH/../config/log4j.properties"
+#LOGBACKOPTS="-Dlogback.configurationFile=file:$SCRIPTPATH/../config/logback.xml"
+STREAMSOPTS="$STREAMSOPTS $LOG4JOPTS $TNT4JOPTS"
+
+if [ "$MAINCLASS" == "" ]; then
+	MAINCLASS="com.jkoolcloud.tnt4j.streams.StreamsAgent"
+fi
+
+java $STREAMSOPTS -classpath "$LIBPATH" $MAINCLASS $*
