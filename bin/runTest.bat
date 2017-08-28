92c71159b115fc4540e8a65a76793bba1bcaeb70
mjokubauskas <mjokubauskas@2638958a-820f-0410-98a6-f3dc9333a13f>
2017-08-28 12:03:38 +0000

    Script fix
    git-svn-id: http://172.16.6.227/svn/new/outsource/Nastel/wmmf_test/tnt4j-streams-ibm-b2bi/trunk@17973 2638958a-820f-0410-98a6-f3dc9333a13f

--- bin/runTest.bat
+++ bin/runTest.bat
@@ -1,7 +1,9 @@
-set LIBPATH="%RUNDIR%\*"
+set RUNDIR=%~dp0
+set LIBPATH="%RUNDIR%..\tnt4j-streams-ibm-b2bi-1.0.0-SNAPSHOT-runnableTest.jar"
 set TNT4JOPTS=-Dtnt4j.config="%RUNDIR%..\config\tnt4j.properties"
 set LOG4JOPTS=-Dlog4j.configuration="file:%RUNDIR%..\config\log4j.properties".
-set EVENTS_DIR=-Dtnt4j.b2biSasmpleEvents="./samples/B2Bi/Events/*.xml"
+set EVENTS_DIR=-Dtnt4j.b2biSasmpleEvents="%RUNDIR%..\samples\B2Bi\Events\*.xml"
 set STREAMSOPTS=%STREAMSOPTS% %LOG4JOPTS% %TNT4JOPTS% %EVENTS_DIR%
 
 java -cp %LIBPATH% %STREAMSOPTS% org.junit.runner.JUnitCore com.jkoolcloud.tnt4j.streams.custom.inputs.B2BiSfgEventStreamTest
+pause
