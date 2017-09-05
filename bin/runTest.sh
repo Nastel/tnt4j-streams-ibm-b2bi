1bde68379d3e249e51aa8d3a34c32545b41893b2
akausinis <akausinis@2638958a-820f-0410-98a6-f3dc9333a13f>
2017-09-05 14:14:30 +0000

    review.
    git-svn-id: http://172.16.6.227/svn/new/outsource/Nastel/wmmf_test/tnt4j-streams-ibm-b2bi/trunk@18041 2638958a-820f-0410-98a6-f3dc9333a13f

--- bin/runTest.sh
+++ bin/runTest.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 RUNDIR=`pwd`
-LIBPATH="$RUNDIR../tnt4j-streams-ibm-b2bi-1.0.1-SNAPSHOT-runnableTest.jar"
+LIBPATH="$RUNDIR../tnt4j-streams-ibm-b2bi-1.0.3-SNAPSHOT-runnableTest.jar"
 TNT4JOPTS=-Dtnt4j.config="$RUNDIR../config/tnt4j.properties"
 LOG4JOPTS=-Dlog4j.configuration="file:/$RUNDIR../config/log4j.properties"
 PARSER_CONFIG=-Dtnt4j.streams.config="$RUNDIR../samples/B2Bi/tnt4j-streams-ibm-b2bi.properties"
