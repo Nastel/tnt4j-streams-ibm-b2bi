5650da648093c909789b88813658b09992c12d3c
akausinis <andrius.kausinis@singleton-labs.lt>
2017-09-14 15:59:36 +0300

    cleanup

--- bin/runTest.sh
+++ bin/runTest.sh
@@ -5,11 +5,11 @@ else
     SCRIPTPATH=$( cd "$(dirname "$0")" ; pwd -P )
 fi
 
-LIBPATH="$SCRIPTPATH../tnt4j-streams-ibm-b2bi-1.0.11-SNAPSHOT-runnableTest.jar"
-TNT4JOPTS="-Dtnt4j.config=$SCRIPTPATH../config/tnt4j.properties"
-LOG4JOPTS="-Dlog4j.configuration=file:/$SCRIPTPATH../config/log4j.properties"
-PARSER_CONFIG="-Dtnt4j.streams.config=$SCRIPTPATH../samples/B2Bi/tnt4j-streams-ibm-b2bi.properties"
-EVENTS_DIR="-Dtnt4j.b2biSampleEvents=$SCRIPTPATH../samples/B2Bi/Events/*.xml"
+LIBPATH="$SCRIPTPATH/../tnt4j-streams-ibm-b2bi-1.0.11-SNAPSHOT-runnableTest.jar"
+TNT4JOPTS="-Dtnt4j.config=$SCRIPTPATH/../config/tnt4j.properties"
+LOG4JOPTS="-Dlog4j.configuration=file:/$SCRIPTPATH/../config/log4j.properties"
+PARSER_CONFIG="-Dtnt4j.streams.config=$SCRIPTPATH/../samples/B2Bi/tnt4j-streams-ibm-b2bi.properties"
+EVENTS_DIR="-Dtnt4j.b2biSampleEvents=$SCRIPTPATH/../samples/B2Bi/Events/*.xml"
 STREAMSOPTS=$STREAMSOPTS $LOG4JOPTS $TNT4JOPTS $EVENTS_DIR $PARSER_CONFIG "-Dtest=true"
 
 java -cp "$LIBPATH" "$STREAMSOPTS" org.junit.runner.JUnitCore com.jkoolcloud.tnt4j.streams.custom.inputs.B2BiSfgEventListenerTest
