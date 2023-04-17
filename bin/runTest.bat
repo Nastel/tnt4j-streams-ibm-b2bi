ed86a794b8fa79f9ff844abb6d11ca3a20a663ba
Andrius Kausinis <andrius.kausinis@singleton-labs.lt>
2023-04-17 09:31:58 +0300

    Made log4j/logback configurationFile to be simple path instead of file URI to proper handle Windows path backslashes.

--- bin/runTest.bat
+++ bin/runTest.bat
@@ -4,7 +4,7 @@ setlocal
 set RUNDIR=%~dp0
 set LIBPATH=%RUNDIR%..\tnt4j-streams-ibm-b2bi-1.0.31-runnableTest.jar
 set TNT4JOPTS=-Dtnt4j.config="%RUNDIR%..\config\tnt4j.properties"
-set LOG4JOPTS=-Dlog4j2.configurationFile="file:///%RUNDIR%..\config\log4j2.xml"
+set LOG4JOPTS=-Dlog4j2.configurationFile="%RUNDIR%..\config\log4j2.xml"
 set PARSER_CONFIG=-Dtnt4j.streams.config="%RUNDIR%..\samples\B2Bi\tnt4j-streams-ibm-b2bi.properties"
 set EVENTS_DIR=-Dtnt4j.b2biSampleEvents="%RUNDIR%..\samples\B2Bi\Events\*.xml"
 set STREAMSOPTS=%STREAMSOPTS% %LOG4JOPTS% %TNT4JOPTS% %EVENTS_DIR% %PARSER_CONFIG%
